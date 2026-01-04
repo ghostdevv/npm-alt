3c11f18e35941b18395b0c9bfdff729288ac4cb4
Willow (GHOST) <git@willow.sh>
2026-01-04 19:03:22 +0000

    refactor: use resolve fn

--- src/routes/(nav)/package/[...specifier]/+page.ts
+++ src/routes/(nav)/package/[...specifier]/+page.ts
@@ -1,5 +1,6 @@
 import { redirect } from '@sveltejs/kit';
+import { resolve } from '$app/paths';
 
 export function load({ params }) {
-	redirect(307, `/package/${params.specifier}/overview`);
+	redirect(307, resolve(`/(nav)/package/[...specifier]/overview`, params));
 }
