450841a446d44b01525dd555ebbc0d1469d47d04
Willow (GHOST) <git@willow.sh>
2026-01-04 19:17:38 +0000

    feat: versions can be clicked on from versions tab

--- src/routes/(nav)/package/[...specifier]/versions/+page.svelte
+++ src/routes/(nav)/package/[...specifier]/versions/+page.svelte
@@ -1,61 +1,71 @@
 <script lang="ts">
-	import { getPackageVersions } from '$lib/data/package.remote.js';
+	import { getPackage, getPackageVersions } from '$lib/data/package.remote';
 	import IconCalendar from 'virtual:icons/lucide/calendar';
 	import { format as formatBytes } from '@std/fmt/bytes';
 	import IconWeight from 'virtual:icons/lucide/weight';
 	import IconScale from 'virtual:icons/lucide/scale';
 	import IconTrash from 'virtual:icons/lucide/trash';
 	import { formatDistanceToNow } from 'date-fns';
+	import { resolve } from '$app/paths';
 
 	const { params } = $props();
 	const versions = $derived(await getPackageVersions(params.specifier));
+	const pkg = $derived(await getPackage(params.specifier));
 </script>
 
 <section>
 	<ol>
 		{#each versions as pkv}
-			<li class={[pkv.groupState]}>
-				{#if pkv.groupState === 'deprecated'}
-					<div class="group" title="This version is deprecated">
-						<IconTrash />
-					</div>
-				{:else}
-					<p
-						class="group"
-						title={pkv.groupState === 'lead'
-							? 'Latest version in range'
-							: undefined}
-					>
-						{pkv.group}
+			<li>
+				<a
+					data-sveltekit-preload-data="tap"
+					href={resolve('/(nav)/package/[...specifier]/overview', {
+						specifier: `${pkg.name}@${pkv.version}`,
+					})}
+					class={[pkv.groupState]}
+				>
+					{#if pkv.groupState === 'deprecated'}
+						<div class="group" title="This version is deprecated">
+							<IconTrash />
+						</div>
+					{:else}
+						<p
+							class="group"
+							title={pkv.groupState === 'lead'
+								? 'Latest version in range'
+								: undefined}
+						>
+							{pkv.group}
+						</p>
+					{/if}
+
+					<p class="version">{pkv.version}</p>
+
+					<p>
+						<IconCalendar />
+						{#if pkv.publishedAt}
+							{formatDistanceToNow(pkv.publishedAt, {
+								addSuffix: true,
+							})}
+						{:else}
+							???
+						{/if}
 					</p>
-				{/if}
 
-				<p class="version">{pkv.version}</p>
+					<p>
+						<IconWeight />
+						{#if pkv.unpackedSize}
+							{formatBytes(pkv.unpackedSize)}
+						{:else}
+							???
+						{/if}
+					</p>
 
-				<p>
-					<IconCalendar />
-					{#if pkv.publishedAt}
-						{formatDistanceToNow(pkv.publishedAt, {
-							addSuffix: true,
-						})}
-					{:else}
-						???
-					{/if}
-				</p>
-
-				<p>
-					<IconWeight />
-					{#if pkv.unpackedSize}
-						{formatBytes(pkv.unpackedSize)}
-					{:else}
-						???
-					{/if}
-				</p>
-
-				<p>
-					<IconScale />
-					{pkv.license ?? '???'}
-				</p>
+					<p>
+						<IconScale />
+						{pkv.license ?? '???'}
+					</p>
+				</a>
 			</li>
 		{/each}
 	</ol>
@@ -65,7 +75,7 @@
 	ol {
 		list-style: none;
 
-		li {
+		a {
 			display: grid;
 			grid-auto-columns: max-content;
 			grid-template-rows: max-content max-content;
@@ -76,20 +86,35 @@
 			padding-block: 8px;
 			padding-inline: 12px;
 
-			background-color: var(--background-secondary);
-			border: 2px solid var(--background-tertiary);
+			background-color: var(--background, var(--background-secondary));
+			transition: background-color 0.2s ease-in-out;
+			border: 2px solid var(--border, var(--background-tertiary));
 			border-radius: 12px;
+			color: var(--text);
 
-			&.deprecated,
-			&.deprecated .group {
-				border-color: var(--red);
-				background-color: color(from var(--red) srgb r g b / 0.2);
+			&.deprecated {
+				--border: var(--red);
+				--background: color(from var(--red) srgb r g b / 0.2);
 			}
 
-			&.lead,
-			&.lead .group {
-				border-color: var(--green);
-				background-color: color(from var(--green) srgb r g b / 0.2);
+			&.lead {
+				--border: var(--green);
+				--background: color(from var(--green) srgb r g b / 0.2);
+			}
+
+			&:active,
+			&:hover {
+				text-decoration: none;
+				/* prettier-ignore */
+				background-color: color(
+					from var(--background, var(--background-secondary)) srgb r g b / 0.3
+				);
+			}
+
+			&:focus-visible {
+				outline: 2px solid var(--border, var(--primary));
+				outline-offset: 2px;
+				text-decoration: none;
 			}
 
 			.version,
@@ -106,8 +131,8 @@
 
 				width: 3.5rem;
 				height: 3.5rem;
-				border: 2px solid var(--background-tertiary);
-				background-color: var(--background-primary);
+				border: 2px solid var(--border, var(--background-tertiary));
+				background-color: var(--background, var(--background-primary));
 				border-radius: 100%;
 			}
 
