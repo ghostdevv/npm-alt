559c0c48af59bdbb3dbd071e9b3711427861493f
Willow (GHOST) <git@willow.sh>
2026-01-07 01:15:38 +0000

    refactor: move global css to it's own file

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
-	import 'ghostsui';
+	import './global.css';
 
 	let { children } = $props();
 </script>
@@ -20,36 +20,5 @@
 		max-width: 1200px;
 		margin: 0 auto;
 		height: 100dvh;
-	}
-
-	/* styles moved here from $lib/boundary.svelte due to */
-	/* https://github.com/sveltejs/svelte/issues/16404 */
-	/* when fixed should move back to component */
-	:global(.boundary) {
-		&.failed,
-		&.pending {
-			display: flex;
-			align-items: center;
-			justify-content: flex-start;
-			height: fit-content;
-			gap: 4px;
-		}
-
-		&.failed {
-			color: var(--red);
-		}
-	}
-
-	:global(.boundary.pending svg),
-	:global(.spin) {
-		animation: spin 1s linear infinite forwards;
-		transform: rotate(0deg);
-		will-change: transform;
-	}
-
-	@keyframes spin {
-		100% {
-			transform: rotate(1turn);
-		}
 	}
 </style>
